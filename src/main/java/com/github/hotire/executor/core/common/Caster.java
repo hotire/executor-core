29c29c52f367e772ae53790d8ff2ee9ab6b116cc
hotire <gngh0101@gmail.com>
2019-07-01 00:35:10 +0900

    add Caster

---
+++ src/main/java/com/github/hotire/executor/core/common/Caster.java
@@ -0,0 +1,25 @@
+package com.github.hotire.executor.core.common;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Caster {
+  private Caster(){}
+  public static <T, R> Consumer<T> castAccept(Function<T, R> castFunction, Consumer<R> consumer) {
+    return t -> consumer.accept(castFunction.apply(t));
+  }
+
+  static class CastConsumer<T, R> {
+    private Function<T, R> castFunction;
+    private CastConsumer(Function<T, R> castFunction) {
+      this.castFunction = castFunction;
+    }
+    public Consumer<T> accept(Consumer<R> consumer) {
+      return t -> consumer.accept(castFunction.apply(t));
+    }
+  }
+
+  public static <T, R> CastConsumer<T, R> cast(Function<T, R> castFunction) {
+    return new CastConsumer<>(castFunction);
+  }
+}
---
+++ src/main/java/com/github/hotire/executor/core/common/ErrorHelper.java
@@ -0,0 +1,18 @@
+package com.kakaocorp.bakery.executor.core.common;
+
+import java.util.function.Consumer;
+
+public class ErrorHelper {
+  private ErrorHelper(){}
+  public static Consumer<Throwable> throwError() {
+    return throwable -> {
+      throw new RuntimeException(throwable);
+    };
+  }
+  public static Consumer<Throwable> acceptAndThrowError(Consumer<Throwable> doOnError) {
+    return throwable -> {
+      doOnError.accept(throwable);
+      throw new RuntimeException(throwable);
+    };
+  }
+}
